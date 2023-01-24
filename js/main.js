a76978ca014ed3cda184354a88c9664f71819409
Burcu Irem Sari <burcu.sari5334@gmail.com>
2023-01-24 21:40:36 +0300

    task ekleme düzenlendi.

--- js/main.js
+++ js/main.js
@@ -1,26 +1,15 @@
 "use strict";
-let list = document.getElementById("to-do-list");
-
-function addtask() {
-  if (document.getElementById("input").value == "") {
-    alert("bir görev giriniz");
+list = document.getElementById("to-do-list");
+function newTask(event) {
+  let li = document.createElement("li");
+  let inputValue = document.getElementById("input").value;
+  let task = document.createTextNode(inputValue);
+  li.appendChild(task);
+  if (inputValue === "") {
+    alert("bir görev girmelisin.");
   } else {
-    list.innerHTML += ` <li id="task">
-      <div>
-        <h3>${document.getElementById("input").value}</h3>
-      </div>
-      <div>
-        <i class="fa-solid fa-pen"></i>
-        <i class="fa-solid fa-trash-can delete"></i>
-      </div>
-    </li>`;
-    var current_tasks = document.querySelectorAll(".delete");
-    for (var i = 0; i < current_tasks.length; i++) {
-      current_tasks[i].onclick = function () {
-        this.parentNode.parentNode.remove();
-      };
-      document.getElementById("input").value = " ";
-      event.preventDefault();
-    }
+    document.getElementById("to-do-list").appendChild(li);
   }
+  document.getElementById("input").value = "";
+  event.preventDefault();
 }
