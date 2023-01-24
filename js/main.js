a38b08ac7f8f61c411f4307bc28192402cf8ebe6
Burcu Irem Sari <burcu.sari5334@gmail.com>
2023-01-24 22:06:29 +0300

    task ekleme güncellendi.
    eklenen task'ın silinmesi ve düzenlenmesi için gerekli icon'lar eklendi.

--- js/main.js
+++ js/main.js
@@ -2,14 +2,17 @@
 list = document.getElementById("to-do-list");
 function newTask(event) {
   let li = document.createElement("li");
+  li.className ="task";
   let inputValue = document.getElementById("input").value;
-  let task = document.createTextNode(inputValue);
-  li.appendChild(task);
+  li.innerHTML = ` <div><span>${inputValue}</span></div>
+  <div><i class="fa-solid fa-trash"></i>
+  <i class="fa-solid fa-pen-to-square"></i></div>`;
   if (inputValue === "") {
     alert("bir görev girmelisin.");
   } else {
     document.getElementById("to-do-list").appendChild(li);
   }
   document.getElementById("input").value = "";
-  event.preventDefault();
+
+
 }
