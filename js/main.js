808122f3fac0ffe40c522b0e7a5be09dba6291cd
Burcu Irem Sari <burcu.sari5334@gmail.com>
2023-01-30 21:17:39 +0300

    yorum satırları güncellenmiştir.

--- js/main.js
+++ js/main.js
@@ -49,10 +49,12 @@
     completed_button.classList.add("completed");
     completed_button.innerHTML = "<i class='fa-regular fa-square-check'></i>";
 
+    // oluşturulan action butonlarını task-action içine eklenmiştir.
     task_div_action.appendChild(delete_button);
     task_div_action.appendChild(edit_button);
     task_div_action.appendChild(completed_button);
 
+    //task'lerin editlenmesi;
     edit_button.addEventListener("click", function () {
       console.log(edit_button.value);
       if (edit_button.value == "edit") {
@@ -68,11 +70,12 @@
       }
     });
 
+    //task'lerin silinmesi;
     delete_button.addEventListener("click", function () {
       tasks_list.removeChild(task_li);
     });
-    input_task.value = "";
 
+    //task'lerin tamamlanması;
     completed_button.addEventListener("click", function () {
       if (added_task.style.textDecoration == "none") {
         added_task.style.textDecoration = "line-through";
@@ -84,10 +87,8 @@
         added_task.style.textDecoration = "none";
         completed_button.style.color = "#333";
         added_task.style.color = "#333"
-
-
-
       }
     });
+    input_task.value = "";
   });
 
